5efd0b596f8113c55e0dec46d11a8863627779ae
cafehaine <kilian.guillaume@gmail.com>
2018-03-04 21:49:55 +0000

    install.sh: set correct permissions

--- install.sh
+++ install.sh
@@ -6,13 +6,15 @@ set -o errexit
 # install bin
 mkdir -p ${INSTALLROOT}/usr/bin
 cp luastatus.lua ${INSTALLROOT}/usr/bin/luastatus
-chmod +x ${INSTALLROOT}/usr/bin/luastatus
+chmod 755 ${INSTALLROOT}/usr/bin/luastatus
 
 # install default config
 mkdir -p ${INSTALLROOT}/etc
 cp default_conf.json ${INSTALLROOT}/etc/luastatus_conf.json
+chmod 644 ${INSTALLROOT}/etc/luastatus_conf.json
 
 # install base modules
 mkdir -p ${INSTALLROOT}/usr/share/luastatus/modules
 cp modules/* ${INSTALLROOT}/usr/share/luastatus/modules/
+chmod 644 ${INSTALLROOT}/usr/share/luastatus/modules/*
 
