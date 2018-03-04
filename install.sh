a1fe2e8dbf1428eb4291d671d3278f8ebc8b7faa
cafehaine <kilian.guillaume@gmail.com>
2018-03-04 15:32:57 +0000

    Improvements over install.sh (still not tested\!)

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# exit on error
+set -o errexit
+
+# install bin
 cp luastatus.lua /usr/bin/luastatus
 chmod +x /usr/bin/luastatus
 
+# install default config
 cp default_conf.json /etc/luastatus_conf.json
 
+# install base modules
 mkdir -p /usr/share/luastatus/modules
 cp modules/* /usr/share/luastatus/modules/
 
