e010a93d6efb142d9cf03538e39a38efa4f78e42
cafehaine <kilian.guillaume@gmail.com>
2018-03-04 21:27:56 +0000

    Let install.sh install in a specific directory

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 set -o errexit
 
 # install bin
-cp luastatus.lua /usr/bin/luastatus
-chmod +x /usr/bin/luastatus
+cp luastatus.lua ${INSTALLROOT}/usr/bin/luastatus
+chmod +x ${INSTALLROOT}/usr/bin/luastatus
 
 # install default config
-cp default_conf.json /etc/luastatus_conf.json
+cp default_conf.json ${INSTALLROOT}/etc/luastatus_conf.json
 
 # install base modules
 mkdir -p /usr/share/luastatus/modules
-cp modules/* /usr/share/luastatus/modules/
+cp modules/* ${INSTALLROOT}/usr/share/luastatus/modules/
 
