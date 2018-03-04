13f8d30f62af2cdda9df9eea913ec1eccf0df622
cafehaine <kilian.guillaume@gmail.com>
2018-03-04 21:31:19 +0000

    install.sh: create directories

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@
 set -o errexit
 
 # install bin
+mkdir -p ${INSTALLROOT}/usr/bin
 cp luastatus.lua ${INSTALLROOT}/usr/bin/luastatus
 chmod +x ${INSTALLROOT}/usr/bin/luastatus
 
 # install default config
+mkdir -p ${INSTALLROOT}/etc
 cp default_conf.json ${INSTALLROOT}/etc/luastatus_conf.json
 
 # install base modules
